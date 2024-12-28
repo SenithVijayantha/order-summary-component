2fb4dfef106ced20e3643658a30e87e08265e2d7
SenithSV <ssvsenith@gmail.com>
2024-12-28 14:16:40 +0530

    Add margin and padding to body, remove background image repeat

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
     screens: {
-      xs: "480px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
+      'xs': "480px",
+      'sm': "640px",
+      'md': "768px",
+      'lg': "1024px",
+      'xl': "1280px",
+      '2xl': "1536px",
     },
     extend: {
       fontFamily: {
